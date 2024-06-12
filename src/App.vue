a3ea3ac33a1296caf1c2b65c31773df957dca5dd
Amr Kahla <amrkahla6@gmail.com>
2024-06-12 19:03:52 +0300

    First application with component

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <active-user :username="users.name" :userage="users.age"></active-user>
+        <user-data @set-data="setUserData"></user-data>
+    </div>
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+                users : {
+                    name : "Abed kahla",
+                    age  : 28
+                }
+            }
+        },methods : {
+            setUserData(name,age)
+            {
+                // this.users.name = updateName;
+                // this.users.age  = updatedAge;
+                this.users = {
+                    name : name,
+                    age  : +age
+                }
+            }
+        }
+    };
+</script>
+
+<style>
+    html{
+        font-family: sans-serif;
+    }
+    section {
+        margin: 2rem auto;
+        max-width: 40rem;
+        border-radius: 12px;
+        border: 1px solid #ccc;
+        padding: 1rem;
+    }
+</style>
---
+++ src/components/UserData.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <form @submit.prevent="submitData">
+            <input type="text" placeholder="Your Name" v-model="enterdName">
+            <input type="text" placeholder="Your Age" v-model="enterdAge">
+            <button>Set User Date</button>
+        </form>
+   
+    </section>
+</template>
+
+<script>
+    export default{
+        emits:['set-data'],
+        data(){
+            return {
+                enterdName : "",
+                enterdAge  : ""
+            }
+        },methods : {
+            submitData()
+            {
+                this.$emit('set-data',this.enterdName,this.enterdAge)
+            }
+        }
+    };
+</script>
